bc867bb08d27bc9ecc009c29d5ff213e4f5cbc38
Erik Luimes <erik.luimes@gmail.com>
2016-05-14 23:23:28 +0200

    Added docs

---
+++ Scripts/generate_docs.sh
@@ -0,0 +1,9 @@
+jazzy \
+  --clean \
+  --author Erik Luimes \
+  --github_url https://github.com/ErikLuimes/MeditationPlus \
+  --module-version 1.0.0 \
+  --xcodebuild-arguments -scheme,MeditationPlus \
+  --module MeditationPlus \
+  --root-url https://github.com/ErikLuimes/MeditationPlus/docs \
+  --output docs
