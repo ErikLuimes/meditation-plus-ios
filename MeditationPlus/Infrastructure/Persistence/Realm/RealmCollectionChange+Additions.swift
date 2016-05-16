941669531fdc1e3716f67ee7274a759dbc13b8cb
Erik Luimes <erik.luimes@gmail.com>
2016-05-16 14:26:31 +0200

    Added info button for profile

---
+++ MeditationPlus/Infrastructure/Persistence/Realm/RealmCollectionChange+Additions.swift
@@ -0,0 +1,9 @@
+//
+//  RealmCollectionChange+Additions.swift
+//  MeditationPlus
+//
+//  Created by Erik Luimes on 16/05/16.
+//  Copyright © 2016 Maya Interactive. All rights reserved.
+//
+
+import Foundation
