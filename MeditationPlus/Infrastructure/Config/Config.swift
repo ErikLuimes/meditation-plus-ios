bf243f4f3a4833fabf089e1d1dec00dfb3f29094
Erik Luimes <erik.luimes@gmail.com>
2016-05-21 15:35:28 +0200

    Config

--- MeditationPlus/Infrastructure/Config/Config.swift
+++ MeditationPlus/Infrastructure/Config/Config.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+
+public struct Config
+{
+    static private let config: NSDictionary = {
+        return NSDictionary(contentsOfURL: R.file.defaultConfigPlist.url()!)!
+    }()
+
+    public struct api
+    {
+        static public private(set) var endpoint: String = {
+            return Config.config.valueForKeyPath("api.endpoint") as? String ?? ""
+        }()
+    }
+    
+    public struct youtube
+    {
+        static public private(set) var key: String = {
+            return Config.config.valueForKeyPath("youtube.key") as? String ?? ""
+        }()
+    }
+    
+    public struct crashlytics
+    {
+        static public private(set) var key: String = {
+            return Config.config.valueForKeyPath("crashlytics.key") as? String ?? ""
+        }()
+        
+        static public private(set) var secret: String = {
+            return Config.config.valueForKeyPath("crashlytics.secret") as? String ?? ""
+        }()
+    }
+    
+    private init()
+    {}
+}
